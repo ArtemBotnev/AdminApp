70a1314752acb05c1539d9e4c00a6fea4f57dce9
artem <arbotnev@yandex.ru>
2018-08-24 12:59:28 +0300

    added disable camera functionaliny

--- app/src/main/java/ru/rs/adminapp/AdminReceiver.kt
+++ app/src/main/java/ru/rs/adminapp/AdminReceiver.kt
@@ -5,10 +5,11 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 
 class AdminReceiver : DeviceAdminReceiver() {
     companion object {
-        val TAG = "AdminReceiver"
+        const val TAG = "AdminReceiver"
 
         fun getComponentName(context: Context): ComponentName =
                 ComponentName(context.applicationContext, AdminReceiver::class.java)
@@ -16,11 +17,19 @@ class AdminReceiver : DeviceAdminReceiver() {
 
     override fun onEnabled(context: Context?, intent: Intent?) {
         Log.i(TAG, "Enabled")
+        showToast(context!!, R.string.app_enable)
+
+        MainActivity.launch(context)
+
         super.onEnabled(context, intent)
     }
 
     override fun onDisabled(context: Context?, intent: Intent?) {
         Log.i(TAG, "Disabled")
+        showToast(context!!, R.string.app_disable)
         super.onDisabled(context, intent)
     }
+
+    private fun showToast(context: Context, sourceId: Int) =
+            Toast.makeText(context, sourceId, Toast.LENGTH_SHORT).show()
 }
--- app/src/main/java/ru/rs/adminapp/MainActivity.kt
+++ app/src/main/java/ru/rs/adminapp/MainActivity.kt
@@ -3,33 +3,68 @@ package ru.rs.adminapp
 import android.app.Activity
 import android.app.admin.DevicePolicyManager
 import android.app.admin.DevicePolicyManager.*
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Switch
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG = "MainActivity"
+
+        fun launch(context: Context) =
+                Intent(context, MainActivity::class.java).also { context.startActivity(it) }
     }
+
+    private lateinit var devicePolicyManager: DevicePolicyManager
+    private lateinit var adminComponentName: ComponentName
+    private var isAdminActive = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            val devicePolicyManager =
-                    this.getSystemService(Activity.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        devicePolicyManager = getSystemService(Activity.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        adminComponentName = AdminReceiver.getComponentName(this)
+        isAdminActive = devicePolicyManager.isAdminActive(adminComponentName)
 
-            val isAdminActive = devicePolicyManager
-                    .isAdminActive(AdminReceiver.getComponentName(this@MainActivity))
+        adjustLayout()
 
-            if (!isAdminActive) askAdminRight()
+        if (savedInstanceState == null && !isAdminActive) askAdminRight()
+    }
+
+    /**
+     * Invoked by clicking on cameraSwitch, activity_main layout
+     */
+    fun changeCameraStatus(view: View) {
+        val switch = view as Switch
+        devicePolicyManager.setCameraDisabled(adminComponentName, !switch.isChecked)
+
+        cameraImage.setImageResource(
+                if (switch.isChecked) R.drawable.ic_cam_enable else R.drawable.ic_cam_disable)
+    }
+
+    private fun adjustLayout() {
+        if (devicePolicyManager.getCameraDisabled(adminComponentName)) {
+            cameraImage.setImageResource(R.drawable.ic_cam_disable)
+            cameraSwitch.isChecked = false
+        } else {
+            cameraImage.setImageResource(R.drawable.ic_cam_enable)
+            cameraSwitch.isChecked = true
         }
     }
 
-    private fun askAdminRight() =
-            Intent(ACTION_ADD_DEVICE_ADMIN).apply {
-                putExtra(EXTRA_DEVICE_ADMIN, AdminReceiver.getComponentName(this@MainActivity))
-                putExtra(EXTRA_ADD_EXPLANATION, "Explanation")
-            }.also { startActivity(it) }
+    private fun askAdminRight() {
+        Intent(ACTION_ADD_DEVICE_ADMIN).apply {
+            putExtra(EXTRA_DEVICE_ADMIN, adminComponentName)
+            putExtra(EXTRA_ADD_EXPLANATION, getString(R.string.explanation))
+        }.also { startActivity(it) }
+
+        finish()
+    }
 }
